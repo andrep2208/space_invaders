3464ce705a11a681c168bf1492eb79d591c9a1d2
Andre Pham <andrepham@Andres-MacBook-Pro.local>
2018-08-14 21:43:27 +1200

    update

--- sketch.js
+++ sketch.js
@@ -1,23 +1,46 @@
-var flow = [];
-var wordSize = 30;
-var numberofflow = 80;
 
+
+function Ship() {
+  this.x = 400;
+  this.y = 580;
+  this.speed = 10;
+
+  this.render = function() {
+    push();
+    fill(255,0,0);
+    noStroke();
+    rect(this.x + 30,this.y -20,20,20);
+    rect(this.x,this.y,80,20);
+    pop();
+  }
+  this.update = function () {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.speed;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.speed;
+    }
+    
+  }
+}
+
+
+
+var ship;
 
 function setup () {
   createCanvas(800,600); 
-  for (let i = 0; i < numberofflow; i++){
-    flow[i] = new Flow();
-  }
+  ship = new Ship();
+ 
 }
 
 
 function draw() {
   background(0);
-
-  for(let i = 0; i < flow.length; i++) {
-   flow[i].render();
-   flow[i].replay();
- }
+  ship.render();
+  ship.update();
 
 
 }
+
+
